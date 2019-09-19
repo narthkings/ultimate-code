e30cfff185db4954d19329d30735880274f594a2
narthcodes <imohdumto@gmail.com>
2019-09-19 09:46:16 +0100

    added poppins font

--- register.php
+++ register.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css">
     <title>Document</title>
@@ -71,10 +72,7 @@
                     <div>
                         <button type="submit" class="btn mt-4" name="register-btn">Sign Up</button>
                     </div>
-                    
-                    <div>
-                        <button type="submit" class="btn mt-4 google-btn">Sign Up with Google</button>
-                    </div>
+                
                 </form>
                 <div class="change"> Already have an Account? <a class="login" href="login.php">Login</a></div>
             </div>
